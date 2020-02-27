92e14cb43838ca543ea218a34de7a510cc9336fc
ZulfiqarAli <zulfi4u95@gmail.com>
2020-02-27 17:42:59 +0500

    safe password generator	, Calculator, and Ruler	 are complete today and working on Unit Converter and commited..

---
+++ app/src/main/java/com/ahmedonics/apps/ultimateutilitymaster/activities/Calculator/StandardCalculator/History.java
@@ -0,0 +1,53 @@
+package com.ahmedonics.apps.ultimateutilitymaster.activities.Calculator.StandardCalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.ahmedonics.apps.ultimateutilitymaster.R;
+
+import java.util.ArrayList;
+
+public class History extends AppCompatActivity {
+
+    private ListView lv;
+    private DBHelper dbHelper;
+    private ArrayList<String> list;
+    private ArrayAdapter<String> adapter;
+    private String calcName="";
+    private String []EmptyList={"There is  no history yet"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+
+        setTitle("History");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+
+        lv = findViewById(R.id.listView);
+        dbHelper=new DBHelper(this);
+        calcName=getIntent().getStringExtra("calcName");
+
+        list=dbHelper.showHistory(calcName);
+
+        if(!list.isEmpty())
+            adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,list);
+        else
+            adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,EmptyList);
+        lv.setAdapter(adapter);
+    }
+
+    public void onClick(View v)
+    {
+        dbHelper.deleteRecords(calcName);
+        adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,EmptyList);
+        lv.setAdapter(adapter);
+    }
+
+}
